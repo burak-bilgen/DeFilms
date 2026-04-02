1dabe7dbaadba76bfacc3bb019a574483cb4493e
Burak Bilgen <burakbilgendeveloper@gmail.com>
2026-04-02 18:48:17 +0300

    Setup project architecture and launch screen

---
+++ DeFilms/App/DeFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeFilmsApp.swift
+//  DeFilms
+//
+//  Created by Burak on 2.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct DeFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
